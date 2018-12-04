f303d1f5a8109c093c5b9af93ea6b6d3de4439ef
iukhov <ivan.ukhov@csssr.com>
2018-12-04 21:47:22 +0400

    added new props to Button

--- components/button/Button.md
+++ components/button/Button.md
@@ -1,8 +1,10 @@
 ```jsx
-<div style={{display: 'flex', justifyContent: 'space-between'}}>
-    <Button isDisabled>Кнопка</Button>
-    <Button>Кнопка</Button>
-    <Button mode='danger'>Кнопка</Button>
+<div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', alignItems: 'flex-start'}}>
+    <Button color="gray" size="medium" hasBorder isDisabled>Кнопка</Button>
+    <Button color="gray" size="medium" hasBorder>Кнопка</Button>
+    <Button color="gray" size="medium" hasBorder mode='danger'>Кнопка</Button>
+    <Button color="lightgray" size="small">Кнопка</Button>
+    <Button color="gray" hasUnderline size="large" >Сохранить</Button>
 </div>
 
 ```
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -5,22 +5,36 @@ import cn from 'classnames';
 interface Props {
     children: string;
     mode?: "danger";
-    isDisabled?: boolean
+    size: "small" | "medium" | "large";
+    color: "gray" | "lightgray";
+    hasUnderline?: boolean;
+    isDisabled?: boolean;
+    hasBorder?: boolean;
     test: (value: string) => void;
 }
 
 export class Button extends React.Component<Props> {
     public render() {
-        const { children, mode, isDisabled } = this.props
+        const { children, mode, size, color, hasUnderline, isDisabled, hasBorder } = this.props
         return (
             <button className={
                 cn({
                     [`button_${mode}`]: mode,
+                    [`button_size-${size}`]: size,
+                    [`button_color-${color}`]: color,
                     [`button_isDisabled`]: isDisabled,
-                }, 
-                'button'
-                )}>
-                {children}
+                    [`button_hasBorder`]: hasBorder,
+                }, 'button'
+                )
+            }>
+                <span className={
+                    cn({
+                        [`button__text_hasUnderline`]: hasUnderline,
+                    }, 'button__text'
+                    )
+                }>
+                    {children}
+                </span>
             </button>
         );
     }

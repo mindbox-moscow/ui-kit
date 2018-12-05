8611cc5fb55c982f6409e91faccd92f10c7c0b7d
iukhov <ivan.ukhov@csssr.com>
2018-12-05 15:48:50 +0400

    edited button

---
+++ components/Btn/Btn.md
@@ -0,0 +1,12 @@
+```jsx
+<div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', alignItems: 'flex-start'}}>
+    <Btn color="gray" size="medium" hasBorder isDisabled>Кнопка</Btn>
+    <Btn color="gray" size="medium" hasBorder>Кнопка</Btn>
+    <Btn color="gray" size="medium" hasBorder mode='danger'>Кнопка</Btn>
+    <Btn color="lightgray" size="small">Кнопка</Btn>
+    <Btn color="gray" hasUnderline size="large" >Сохранить</Btn>
+</div>
+```
+
+
+Моя кнопка
--- components/Btn/Btn.tsx
+++ components/Btn/Btn.tsx
@@ -1,36 +1,37 @@
 import * as React from "react";
-import './Button.scss'
+import './Btn.scss';
 import cn from 'classnames';
 
 interface Props {
     children: string;
+    className?: string;
     mode?: "danger";
     size: "small" | "medium" | "large";
     color: "gray" | "lightgray";
     hasUnderline?: boolean;
     isDisabled?: boolean;
     hasBorder?: boolean;
-    test: (value: string) => void;
+    test?: (value: string) => void;
 }
 
-export class Button extends React.Component<Props> {
+export class Btn extends React.Component<Props> {
     public render() {
-        const { children, mode, size, color, hasUnderline, isDisabled, hasBorder } = this.props
+        const { children, className, mode, size, color, hasUnderline, isDisabled, hasBorder } = this.props
         return (
             <button className={
-                cn({
+                cn('button', className, {
                     [`button_${mode}`]: mode,
                     [`button_size-${size}`]: size,
                     [`button_color-${color}`]: color,
                     [`button_isDisabled`]: isDisabled,
                     [`button_hasBorder`]: hasBorder,
-                }, 'button'
+                }
                 )
             }>
                 <span className={
-                    cn({
+                    cn('button__text', {
                         [`button__text_hasUnderline`]: hasUnderline,
-                    }, 'button__text'
+                    }
                     )
                 }>
                     {children}

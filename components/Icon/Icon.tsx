81c076f6fe618882472978a12c10720f368cbf9d
iukhov <ivan.ukhov@csssr.com>
2018-12-06 14:08:33 +0400

    added Icon

---
+++ components/Icon/Icon.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import './Icon.scss';
+
+import cn from 'classnames';
+
+interface Props {
+    className?: string;
+    icon: "play" | "pause";
+}
+
+export class Icon extends React.Component<Props> {
+    public render() {
+        const { icon, className } = this.props
+        return (
+            <div className={cn('icon',
+                {
+                    [`icon_${icon}`]: icon 
+                }, className)
+            }/>
+        );
+    }
+}

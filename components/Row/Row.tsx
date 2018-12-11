68ac95df61ad0c090d699d8fc1d8280629b6dedb
iukhov <ivan.ukhov@csssr.com>
2018-12-11 18:42:58 +0400

    edit after review

--- components/Row/Row.tsx
+++ components/Row/Row.tsx
@@ -4,20 +4,19 @@ import cn from 'classnames'
 import { FieldName } from '../FieldName/FieldName';
 
 interface Props {
-    className?: string;
     title: string;
     isEdit?: boolean;
 }
 
 export class Row extends React.Component<Props> {
     public render() {
-        const { children, className, title, isEdit } = this.props
+        const { children, title, isEdit } = this.props
         return (
             <div className={cn(
                 {
                     ['row']: true,
                     ['row_isEdit']: isEdit
-                }, className
+                }
             )}>
                 <FieldName isEdit={isEdit}>{title}</FieldName>
                 <div className="row__content">{children}</div>

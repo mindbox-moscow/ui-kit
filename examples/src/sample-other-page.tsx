c1fc167f2387e35027a27e2ef5559162e86e6057
Artem Sakhatskiy <sakhatskiy@yahoo.com>
2018-12-12 16:24:52 +0300

    build: build examples with npm package

---
+++ examples/src/sample-other-page.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { SectionWrapper } from "@mindbox/ui-kit/components/SectionWrapper";
+import { Button } from "@mindbox/ui-kit/components/Button";
+
+const entryElement = document.createElement("div");
+document.body.appendChild(entryElement);
+
+ReactDOM.render(
+    <SectionWrapper title="Tiitle" mode="isEdit" >
+        <p>Test other</p>
+        <Button size="large" color="lightgray">Test</Button>
+        <p>Other test</p>
+    </SectionWrapper>,
+    entryElement
+)

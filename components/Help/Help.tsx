cbebf455bec9664a7ae85b46562323e01c1bd6b4
Unknown <igor.frontend@gmail.com>
2018-12-14 13:10:45 +0300

    build

--- components/Help/Help.tsx
+++ components/Help/Help.tsx
@@ -2,14 +2,15 @@ import * as React from "react";
 import './Help.scss'
 import { Icon } from '../Icon/Icon'
 
-interface Props {
-}
-
-export class Help extends React.Component<Props> {
+export class Help extends React.Component<{}> {
     public render() {
+        const { children } = this.props;
         return (
             <div className='help'>
                 <Icon icon='help__icon' />
+                <div className='help__inner'>
+                    {children}
+                </div>
             </div>
         );
     }

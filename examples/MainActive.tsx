8c0cbc229ffacdca8c837299a77a55010f929676
Artem Sakhatskiy <sakhatskiy@yahoo.com>
2018-12-21 21:50:15 +0300

    🔧: build examples with common webpack config

--- examples/MainActive.tsx
+++ examples/MainActive.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
-import { What } from "@mindbox/ui-kit/templates/What/What";
+import { MainActive } from "../templates/MainActive/MainActive";
 
 const entryElement = document.createElement("div");
 document.body.appendChild(entryElement);
 
 ReactDOM.render(
-    <What />,
+    <MainActive />,
     entryElement
 )
--- examples/Whom.tsx
+++ examples/Whom.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Whom } from "@mindbox/ui-kit/templates/Whom/Whom";
+import { Whom } from "../templates/Whom/Whom";
 
 const entryElement = document.createElement("div");
 document.body.appendChild(entryElement);

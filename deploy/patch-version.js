4da58b0418e85799aa35b9bcf257ea581fc3abc4
Artem Sakhatskiy <sakhatskiy@yahoo.com>
2018-12-21 18:28:12 +0300

    📦: add push to gh-pages to travis config

--- deploy/patch-version.js
+++ deploy/patch-version.js
@@ -2,7 +2,7 @@ const execSync = require("child_process").execSync;
 const semver = require("semver");
 const fs = require("fs");
 
-const exec = (command) => execSync(command, {encoding: 'utf8'});
+const exec = (command) => execSync(command, { encoding: 'utf8' });
 
 const version = exec("npm show @mindbox/ui-kit version");
 
@@ -15,4 +15,4 @@ package.version = patchedVersion;
 fs.writeFileSync("./package.json", JSON.stringify(package, null, 2));
 
 console.log("Current npm version:", version);
-console.log("Publishing version:", patchedVersion, JSON.stringify(package, null, 2));
+console.log("Publishing version:", patchedVersion);

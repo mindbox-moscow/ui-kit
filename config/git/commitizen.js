5fe88f68e28fd28192b74bed2c03cf21b5a2032f
Artem Sakhatskiy <sakhatskiy@yahoo.com>
2018-12-21 17:12:42 +0300

    📝: update cz templates

--- config/git/commitizen.js
+++ config/git/commitizen.js
@@ -1,73 +1,68 @@
 "use strict";
 
 module.exports = {
-  types: [
-    {
-      value: "build",
-      name: "build:     Project build and dependency management"
+    types: [{
+            value: "🔧",
+            name: "build:     Project build and dependency management"
+        },
+        { value: "📦", name: "ci:        Updating CI and scripts" },
+        { value: "📝", name: "docs:      Documentation only changes" },
+        { value: "🍏", name: "feat:      A new feature" },
+        { value: "🐛", name: "fix:       A bug fix" },
+        {
+            value: "⌚️",
+            name: "perf:      Performance changes"
+        },
+        {
+            value: "🌈",
+            name: "refactor:  Code refactoring without feature changes"
+        },
+        { value: "◀️", name: "revert:    Commit reverts" },
+        {
+            value: "🎨",
+            name: "style:     Codestyle changes (tabs, spaces, semicolons, etc.)"
+        },
+        { value: "✅", name: "test:      Adding tests" }
+    ],
+
+    // Область. Она характеризует фрагмент кода, которую затронули изменения
+    scopes: [
+        { name: "components" },
+    ],
+
+    // Возможность задать спец ОБЛАСТЬ для определенного типа коммита (пример для 'fix')
+    /*
+    scopeOverrides: {
+      fix: [
+        {name: 'style'},
+        {name: 'e2eTest'},
+        {name: 'unitTest'}
+      ]
     },
-    { value: "ci", name: "ci:        Updating CI and scripts" },
-    { value: "docs", name: "docs:      Documentation only changes" },
-    { value: "feat", name: "feat:      A new feature" },
-    { value: "fix", name: "fix:       A bug fix" },
-    {
-      value: "perf",
-      name: "perf:      Performance changes"
+    */
+
+    // Поменяем дефолтные вопросы
+    messages: {
+        type: "What changes have you done?",
+        scope: "\nChoose SCOPE (optional):",
+        // Спросим если allowCustomScopes в true
+        customScope: "Type custom SCOPE:",
+        subject: "Write SHORT title in imperative mood:\n",
+        body: 'Write EXTENDED description (optional). Use "|" to separate lines:\n',
+        breaking: "BREAKING CHANGES (optional):\n",
+        footer: "You can place PR and issue links here: #72, #481:\n",
+        confirmCommit: "Confirm commit?"
     },
-    {
-      value: "refactor",
-      name:
-        "refactor:  Code refactoring without feature changes"
-    },
-    { value: "revert", name: "revert:    Commit reverts" },
-    {
-      value: "style",
-      name:
-        "style:     Codestyle changes (tabs, spaces, semicolons, etc.)"
-    },
-    { value: "test", name: "test:      Adding tests" }
-  ],
 
-  // Область. Она характеризует фрагмент кода, которую затронули изменения
-  scopes: [
-    { name: "components" },
-  ],
+    // Разрешим собственную ОБЛАСТЬ
+    allowCustomScopes: true,
 
-  // Возможность задать спец ОБЛАСТЬ для определенного типа коммита (пример для 'fix')
-  /*
-  scopeOverrides: {
-    fix: [
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
+    // Запрет на Breaking Changes
+    allowBreakingChanges: false,
 
-  // Поменяем дефолтные вопросы
-  messages: {
-    type: "What changes have you done?",
-    scope: "\nChoose SCOPE (optional):",
-    // Спросим если allowCustomScopes в true
-    customScope: "Type custom SCOPE:",
-    subject: "Write SHORT title in imperative mood:\n",
-    body:
-      'Write EXTENDED description (optional). Use "|" to separate lines:\n',
-    breaking: "BREAKING CHANGES (optional):\n",
-    footer:
-      "You can place PR and issue links here: #72, #481:\n",
-    confirmCommit: "Confirm commit?"
-  },
+    // Префикс для нижнего колонтитула
+    footerPrefix: "LINKS:",
 
-  // Разрешим собственную ОБЛАСТЬ
-  allowCustomScopes: true,
-
-  // Запрет на Breaking Changes
-  allowBreakingChanges: false,
-
-  // Префикс для нижнего колонтитула
-  footerPrefix: "LINKS:",
-
-  // limit subject length
-  subjectLimit: 72
+    // limit subject length
+    subjectLimit: 72
 };

455ce81c3b282d3f1828a360fe95b788f750c131
CubiZm <karelense@gmail.com>
2019-06-14 18:09:16 +0300

    added new Input props

--- src/Input/Input.md
+++ src/Input/Input.md
@@ -2,6 +2,8 @@
 <div style={{ width: "42px" }}>
     <Input />
     <br />
+    <Input classname="no-shadow" />
+    <br />
     <div>
         <Input defaultValue="10" maxLength={2} />
     </div>
--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import cn from "classnames"
 import "./Input.scss";
 
 interface Props {
@@ -6,17 +7,23 @@ interface Props {
     type?: string;
     maxLength?: number;
     onChange?: (event: React.FormEvent<HTMLInputElement>) => void;
+	classname?: string;
 }
 
 export class Input extends React.Component<Props> {
     public render() {
-        const { defaultValue, type, onChange, maxLength } = this.props;
+        const { defaultValue, type, onChange, maxLength, classname } = this.props;
 
         return (
             <input
                 onChange={onChange}
                 type={type}
-                className="kit-input-field"
+				className={cn(
+					"kit-input-field",
+					{
+						[`kit-input-field_${classname}`]: classname
+					}
+				)}
                 defaultValue={defaultValue}
                 maxLength={maxLength}
             />

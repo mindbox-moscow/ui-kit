d959183b7c3640aedebb9584ee9c6ac5f30f03b3
Костя Бузорин <kostya.buzorin@gmail.com>
2019-06-18 19:21:11 +0300

    Добавлена иконка close

--- src/IconSvg/assets/index.ts
+++ src/IconSvg/assets/index.ts
@@ -1,14 +1,16 @@
 import catalogTree from "./catalog-tree.svg";
+import close from "./close.svg";
 import coins from "./coins.svg";
 import dots from "./dots.svg";
 import percentRound from "./percent-round.svg";
 
-type IconType = "catalog-tree" | "coins" | "dots" | "percent-round";
+type IconType = "catalog-tree" | "close" | "coins" | "dots" | "percent-round";
 
 type AssetsType = { [key in IconType]: string };
 
 const assets: AssetsType = {
 	"catalog-tree": catalogTree,
+	close,
 	coins,
 	dots,
 	"percent-round": percentRound

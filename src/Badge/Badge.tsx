1e8e3b697454ee8e0c4a5a6c36806b77a292c166
Костя Бузорин <kostya.buzorin@gmail.com>
2019-06-20 10:16:07 +0300

    Фикс форматирования, 4 пробела заменены на таб

--- src/Badge/Badge.tsx
+++ src/Badge/Badge.tsx
@@ -7,44 +7,44 @@ import cn from "classnames";
 type Mode = "success" | "danger" | "warning" | "disabled" | "ghost";
 
 const ModeColors = {
-    danger: COLORS.Danger,
-    disabled: COLORS.Disabled,
-    ghost: "transparent",
-    success: COLORS.Success,
-    warning: COLORS.Warning
+	danger: COLORS.Danger,
+	disabled: COLORS.Disabled,
+	ghost: "transparent",
+	success: COLORS.Success,
+	warning: COLORS.Warning
 };
 
 interface IProps {
-    title: string;
-    color?: COLORS;
-    date?: string;
-    size?: string;
-    mode?: Mode;
+	title: string;
+	color?: COLORS;
+	date?: string;
+	size?: string;
+	mode?: Mode;
 }
 
 export const Badge = (props: IProps) => {
-    const { title, color = COLORS.Green, date, size, mode } = props;
+	const { title, color = COLORS.Green, date, size, mode } = props;
 
-    return (
-        <div
-            className={cn("kit-badge", {
-                [`kit-badge_size_${size}`]: size,
-                [`kit-badge_mode_${mode}`]: mode
-            })}
-            style={{ backgroundColor: mode ? ModeColors[mode] : color }}
-        >
-            <span
-                className={cn("kit-badge__text", {
-                    "kit-badge__text_withPoint": date
-                })}
-            >
-                {title}
-            </span>
-            {date && (
-                <>
-                    Запущен:<span className="kit-badge__date"> {date}</span>
-                </>
-            )}
-        </div>
-    );
+	return (
+		<div
+			className={cn("kit-badge", {
+				[`kit-badge_size_${size}`]: size,
+				[`kit-badge_mode_${mode}`]: mode
+			})}
+			style={{ backgroundColor: mode ? ModeColors[mode] : color }}
+		>
+			<span
+				className={cn("kit-badge__text", {
+					"kit-badge__text_withPoint": date
+				})}
+			>
+				{title}
+			</span>
+			{date && (
+				<>
+					Запущен:<span className="kit-badge__date"> {date}</span>
+				</>
+			)}
+		</div>
+	);
 };

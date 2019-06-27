9824660bee5ae11fd0bb9beeb204d288f8124d2c
CubiZm <karelense@gmail.com>
2019-06-27 15:42:59 +0300

    Fix bugs

--- src/StockInfo/StockItem/StockItem.tsx
+++ src/StockInfo/StockItem/StockItem.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import cn from 'classnames'
+import cn from "classnames";
+import { ActionsDropdown } from "../../ActionsDropdown";
 import { Badge } from "../../Badge";
 //import { Icon } from "../../Icon";
 import { IconSvg } from "../../IconSvg";
@@ -7,8 +8,8 @@ import { IconSvg } from "../../IconSvg";
 import "./StockItem.scss";
 
 interface Props {
-    title: string;
-	icon: string,
+	title: string;
+	icon: string;
 	start: string | null;
 	finish: string | null;
 	isFinished?: boolean;
@@ -18,33 +19,89 @@ interface Props {
 }
 
 export class StockItem extends React.Component<Props> {
-    public render() {
-    	const {title, start, icon, finish, isFinished, badgeTitle, size, mode} = this.props;
-        return (
-            <div className={cn("kit-stock-item", {
-				"kit-stock-item_finished": isFinished })}>
+	public render() {
+		const {
+			title,
+			start,
+			icon,
+			finish,
+			isFinished,
+			badgeTitle,
+			size,
+			mode
+		} = this.props;
+		return (
+			<div
+				className={cn("kit-stock-item", {
+					"kit-stock-item_finished": isFinished
+				})}
+			>
 				<div className="kit-stock-item__title-wrap">
-					<span className="kit-stock-item__name">
-						{title}
-					</span>
+					<span className="kit-stock-item__name">{title}</span>
 					<div className="kit-stock-item__btn-wrap">
 						<Badge title={badgeTitle} size={size} mode={mode} />
 					</div>
 				</div>
 				<div className="kit-stock-item__promo">
-					<p className="kit-stock-item__promo-title-wrap">
-						<IconSvg type={icon}/>
-						<span className={cn("kit-stock-item__promo-title", {
-						"kit-stock-item__promo-title_no-sale": !start })}>
-							{start ? `C ${start}` : `без даты старта`}
+					<div className="kit-stock-item__date-wrap">
+						<p className="kit-stock-item__promo-title-wrap">
+							<IconSvg type={icon} className={cn("kit-stock-item__icon", {
+								"kit-stock-item__icon_grey": !start
+							})} />
+							<span
+								className={cn("kit-stock-item__promo-title", {
+									"kit-stock-item__promo-title_no-sale": !start
+								})}
+							>
+								{start ? `C ${start}` : `Без даты старта`}
+							</span>
+						</p>
+						<span
+							className={cn("kit-stock-item__sale", {
+								"kit-stock-item__sale_no-sale": !finish
+							})}
+						>
+							{finish ? `по ${finish}` : `без даты окончания`}
 						</span>
-					</p>
-					<span className={cn("kit-stock-item__sale", {
-							"kit-stock-item__sale_no-sale": !finish })}>
-						{finish ? `по ${finish}` : `без даты окончания`}
-					</span>
+					</div>
+
+					<ActionsDropdown
+						className="kit-stock-item__action-dropdown"
+						toggleBtnText="Действия"
+					>
+						<ActionsDropdown.Action
+							title="Редактировать"
+							onClick={() => console.log("Редактировать")}
+						/>
+						<ActionsDropdown.Group title="Добавить">
+							<ActionsDropdown.Action
+								title="Акцию"
+								onClick={() => console.log("Добавить акцию")}
+							/>
+							<ActionsDropdown.Action
+								title="Подгруппу"
+								onClick={() =>
+									console.log("Добавить подгруппу")
+								}
+							/>
+						</ActionsDropdown.Group>
+						<ActionsDropdown.Group title="Акции в группе">
+							<ActionsDropdown.Action
+								title="Остановить"
+								onClick={() => console.log("Остановить")}
+							/>
+							<ActionsDropdown.Action
+								title="Запустить"
+								onClick={() => console.log("Запустить")}
+							/>
+							<ActionsDropdown.Action
+								title="Архивировать"
+								onClick={() => console.log("Архивировать")}
+							/>
+						</ActionsDropdown.Group>
+					</ActionsDropdown>
 				</div>
 			</div>
-        );
-    }
+		);
+	}
 }

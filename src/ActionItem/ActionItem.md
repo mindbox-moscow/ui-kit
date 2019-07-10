445b847f4205e3880ef73e8169eb02212eaa9950
CubiZm <karelense@gmail.com>
2019-07-10 18:53:08 +0300

    New action item

--- src/ActionItem/ActionItem.md
+++ src/ActionItem/ActionItem.md
@@ -33,15 +33,20 @@
         flexDirection: "column"
     }}
 >
-    <ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='in_development'
-    type='discount'/>
-    <ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='in_development'
-                                     type='discount'/>
-                                     
-                                     <div className="group-action">Группа акций</div>
-    <ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='in_development'
-        type='discount'/>
-        <ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='in_development'
-                                         type='discount'/>
+	<ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='in_development'
+type='discount'
+/>
+	<ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='in_development'
+type='discount' lastBeforeGroup={true}
+/>
+
+	<div className="group-action">Группа акций</div>
+
+	<ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='ended'
+type='discount'
+/>
+	<ActionItem  title={'Другая акция'} startDate={'25.05.18'} endDate={'25.11.19'} status='in_development'
+type='discount'
+/>
 </div>
 ```
--- src/ActionItem/ActionItem.tsx
+++ src/ActionItem/ActionItem.tsx
@@ -1,47 +1,53 @@
 import * as React from "react";
 import cn from "classnames";
 
-import "./ActionItem.scss";
+import { ActionsDropdown } from "../ActionsDropdown";
 import { Badge } from "../Badge";
 import { IconSvg } from "../IconSvg";
+import { Tooltip } from "../Tooltip";
 
-type ActionItemType = 'discount' | 'bonus';
+import "./ActionItem.scss";
+
+type ActionItemType = "discount" | "bonus";
 
 type StatusType = "start" | "end";
 
 const StatusMap = {
 	in_development: {
 		title: "В разработке",
-		mode: 'ghost'
+		mode: "ghost"
 	},
-	ended:  {
+	ended: {
 		title: "Завершена",
-		mode: 'disabled'
+		mode: "disabled"
 	},
 	started: {
 		title: "Запущена",
-		mode: 'success'
+		mode: "success"
 	},
 	started_test: {
 		title: "Запущена (тест)",
-		mode: 'warning',
+		mode: "warning"
 	},
 	stopped: {
 		title: "Остановлена",
-		mode: 'danger'
+		mode: "danger"
 	}
 };
 
 interface IProps {
 	title: string;
-	type: ActionItemType,
+	type: ActionItemType;
 	description: string;
 	startDate: string | null;
 	endDate: string | null;
 	status: StatusType;
+	lastBeforeGroup: boolean;
 }
 
 export class ActionItem extends React.Component<IProps> {
+	private StatusType: string;
+
 	public render() {
 		const {
 			title,
@@ -49,22 +55,36 @@ export class ActionItem extends React.Component<IProps> {
 			startDate,
 			endDate,
 			status,
+			lastBeforeGroup
 		} = this.props;
 		return (
 			<div
 				className={cn("kit-stock-item", {
-					"kit-stock-item_finished": status
+					"kit-stock-item_finished": this.StatusType === 'end',
+					"kit-stock-item_last-before-group": lastBeforeGroup
 				})}
 			>
 				<div className="kit-stock-item__title-wrap">
 					<span className="kit-stock-item__name">{title}</span>
 					<div className="kit-stock-item__btn-wrap">
-						<Badge title={StatusMap[status].title} size="small" mode={StatusMap[status].mode} />
+						<Tooltip title="Описание">
+							Раздаем промокод за регистрацию на сайте, при
+							использовании делаем скидку в размере 15%.
+						</Tooltip>
+						<Badge
+							title={StatusMap[status].title}
+							size="small"
+							mode={StatusMap[status].mode}
+						/>
 					</div>
 				</div>
 				<div className="kit-stock-item__promo">
 					<p className="kit-stock-item__promo-title-wrap">
-						<IconSvg type={type === 'discount' ? 'percent-round' : 'coins'} />
+						<IconSvg
+							type={
+								type === "discount" ? "percent-round" : "coins"
+							}
+						/>
 						<span
 							className={cn("kit-stock-item__promo-title", {
 								"kit-stock-item__promo-title_no-sale": !startDate
@@ -81,6 +101,39 @@ export class ActionItem extends React.Component<IProps> {
 						{endDate ? `по ${endDate}` : `без даты окончания`}
 					</span>
 				</div>
+				<ActionsDropdown
+					className="kit-stock-item__action-dropdown"
+					toggleBtnText="Действия"
+				>
+					<ActionsDropdown.Action
+						title="Редактировать"
+						onClick={() => console.log("Редактировать")}
+					/>
+					<ActionsDropdown.Group title="Добавить">
+						<ActionsDropdown.Action
+							title="Акцию"
+							onClick={() => console.log("Добавить акцию")}
+						/>
+						<ActionsDropdown.Action
+							title="Подгруппу"
+							onClick={() => console.log("Добавить подгруппу")}
+						/>
+					</ActionsDropdown.Group>
+					<ActionsDropdown.Group title="Акции в группе">
+						<ActionsDropdown.Action
+							title="Остановить"
+							onClick={() => console.log("Остановить")}
+						/>
+						<ActionsDropdown.Action
+							title="Запустить"
+							onClick={() => console.log("Запустить")}
+						/>
+						<ActionsDropdown.Action
+							title="Архивировать"
+							onClick={() => console.log("Архивировать")}
+						/>
+					</ActionsDropdown.Group>
+				</ActionsDropdown>
 			</div>
 		);
 	}

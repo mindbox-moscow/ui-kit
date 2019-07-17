2658e065aa420f0be8acd781c7ea8d790f50dc36
CubiZm <karelense@gmail.com>
2019-07-17 14:01:19 +0300

    Fixed SVG-code to JSX-format

--- src/IconSvg/icons/Dots.tsx
+++ src/IconSvg/icons/Dots.tsx
@@ -4,16 +4,15 @@ import { ISvgProps } from "./index";
 
 export const Dots = ({ className }: ISvgProps) => (
 	<svg
+		height={16}
+		width={4}
 		className={className}
-		height="16"
-		viewBox="0 0 4 16"
-		width="4"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<g fill="#8aacc2" fill-rule="evenodd">
-			<circle cx="2" cy="2" r="2" />
-			<circle cx="2" cy="14" r="2" />
-			<circle cx="2" cy="8" r="2" />
+		<g fill="#8aacc2" fillRule="evenodd">
+			<circle cx={2} cy={2} r={2} />
+			<circle cx={2} cy={14} r={2} />
+			<circle cx={2} cy={8} r={2} />
 		</g>
 	</svg>
 );

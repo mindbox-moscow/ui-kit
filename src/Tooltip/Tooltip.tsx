eb036c4d3254deace03520735274bc9476c1a55d
Вадим Мишхожев <vadim.stug@gmail.com>
2019-09-30 20:44:42 +0700

    Тултип изменен на функциональный компонент, удалена логика отображения тултипа по клику, добавлен ховер

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -14,62 +14,36 @@ interface DefaultProps {
 	textDecoration: boolean;
 }
 
-export class Tooltip extends React.Component<IProps, IState> {
-	static defaultProps: DefaultProps = {
-		position: "bottom",
-		textDecoration: true
-	};
+type ToolTipProps = IState & IProps;
 
-	public state = {
-		isVisible: false
-	};
-
-	public wrapRef = React.createRef<HTMLDivElement>();
-
-	public handleClick = () =>
-		this.setState(({ isVisible }) => ({ isVisible: !isVisible }));
-
-	public handleClickOutside = (e: MouseEvent) => {
-		const dropdownWrap = this.wrapRef.current!;
-
-		if (!dropdownWrap.contains(e.target as Node)) {
-			this.setState({ isVisible: false });
-		}
-	};
-
-	public componentDidMount() {
-		document.addEventListener("click", this.handleClickOutside);
-	}
-
-	public componentWillUnmount() {
-		document.removeEventListener("click", this.handleClickOutside);
-	}
-
-	public render() {
-		const { textDecoration, position, title, children } = this.props;
-		const { isVisible } = this.state;
-
-		return (
-			<span className="kit-tooltip" ref={this.wrapRef}>
-				<span
-					className={cn("kit-tooltip__title", {
-						"kit-tooltip__title_text-transform": textDecoration
-					})}
-					onClick={this.handleClick}
-				>
-					{title}
-				</span>
-				{isVisible && (
-					<span
-						className={cn(
-							"kit-tooltip__content",
-							`kit-tooltip__content_${position}`
-						)}
-					>
-						{children}
-					</span>
-				)}
+export const Tooltip: React.FC<ToolTipProps> = ({
+	textDecoration,
+	position,
+	title,
+	children
+}) => {
+	return (
+		<span className="kit-tooltip">
+			<span
+				className={cn("kit-tooltip__title", {
+					"kit-tooltip__title_text-transform": textDecoration
+				})}
+			>
+				{title}
 			</span>
-		);
-	}
-}
+			<span
+				className={cn(
+					"kit-tooltip__content",
+					`kit-tooltip__content_${position}`
+				)}
+			>
+				{children}
+			</span>
+		</span>
+	);
+};
+
+Tooltip.defaultProps = {
+	position: "bottom",
+	textDecoration: true
+};

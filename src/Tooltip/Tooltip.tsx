51ec5dac4a2c1f3018aeba550eb6c3c6e2724161
Вадим Мишхожев <vadim.stug@gmail.com>
2019-10-29 15:13:32 +0700

    Исправлен тултип

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -1,49 +1,81 @@
 import cn from "classnames";
 import * as React from "react";
+import { OverflowVisibleContainer } from "../OverflowVisibleContainer";
 import "./Tooltip.scss";
 
 type IProps = {
 	title: string;
 } & Partial<DefaultProps>;
-interface IState {
-	isVisible: boolean;
-}
 
 interface DefaultProps {
 	position: "top" | "bottom";
 	textDecoration: boolean;
 }
 
-type ToolTipProps = IState & IProps;
+type ToolTipProps = IProps;
 
-export const Tooltip: React.FC<ToolTipProps> = ({
-	textDecoration,
-	position,
-	title,
-	children
-}) => {
-	return (
-		<span className="kit-tooltip">
-			<span
-				className={cn("kit-tooltip__title", {
-					"kit-tooltip__title_text-transform": textDecoration
-				})}
-			>
-				{title}
-			</span>
-			<span
-				className={cn(
-					"kit-tooltip__content",
-					`kit-tooltip__content_${position}`
-				)}
-			>
-				{children}
-			</span>
-		</span>
-	);
-};
+export class Tooltip extends React.Component<ToolTipProps> {
+	static defaultProps: DefaultProps = {
+		position: "bottom",
+		textDecoration: true
+	};
 
-Tooltip.defaultProps = {
-	position: "bottom",
-	textDecoration: true
-};
+	public refTitle = React.createRef<HTMLSpanElement>();
+	public refContent = React.createRef<HTMLSpanElement>();
+
+	public componentDidMount() {
+		const refTitle = this.refTitle.current;
+
+		if (refTitle) {
+			refTitle.addEventListener("mouseover", this.handleHoverTitle);
+			refTitle.addEventListener("mouseout", this.handleHoverTitle);
+		}
+	}
+
+	public componentWillUnmount() {
+		const refTitle = this.refTitle.current;
+
+		if (refTitle) {
+			refTitle.removeEventListener("mouseover", this.handleHoverTitle);
+			refTitle.removeEventListener("mouseout", this.handleHoverTitle);
+		}
+	}
+
+	public handleHoverTitle = () => {
+		const refContent = this.refContent.current;
+
+		if (refContent) {
+			refContent.classList.toggle("kit-tooltip__content_show");
+		}
+	};
+
+	public render() {
+		const { textDecoration, position, title, children } = this.props;
+		return (
+			<span className="kit-tooltip">
+				<span
+					ref={this.refTitle}
+					className={cn("kit-tooltip__title", {
+						"kit-tooltip__title_text-transform": textDecoration
+					})}
+				>
+					{title}
+				</span>
+				<OverflowVisibleContainer
+					parentRef={this.refTitle}
+					className="kit-tooltip__popup"
+				>
+					<span
+						ref={this.refContent}
+						className={cn(
+							"kit-tooltip__content",
+							`kit-tooltip__content_${position}`
+						)}
+					>
+						{children}
+					</span>
+				</OverflowVisibleContainer>
+			</span>
+		);
+	}
+}

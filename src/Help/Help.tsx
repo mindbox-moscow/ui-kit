b38d323f81e4ade7bb60f9e1b71af168e7890222
Olga Polikashina <olga.polikashina@gmail.com>
2020-01-22 15:11:41 +0300

    Add icons for statistics table

--- src/Help/Help.tsx
+++ src/Help/Help.tsx
@@ -1,24 +1,24 @@
 import * as React from "react";
 import "./Help.scss";
-import { Icon } from "../Icon/Icon";
+import { IconSvg } from "../IconSvg/IconSvg";
 
-import cn from 'classnames';
+import cn from "classnames";
 
 interface Props {
-    className?: string;
-    children?: any;
+	className?: string;
+	children?: any;
 }
 
 export class Help extends React.Component<Props> {
-    public render() {
-        const { children, className } = this.props;
-        return (
-            <div className={cn("kit-help", className)}>
-                <div className="kit-help__icon">
-                    <Icon icon="help" />
-                </div>
-                <div className="kit-help__inner">{children}</div>
-            </div>
-        );
-    }
+	public render() {
+		const { children, className } = this.props;
+		return (
+			<div className={cn("kit-help", className)}>
+				<div className="kit-help__icon">
+					<IconSvg type="question" size="large" />
+				</div>
+				<div className="kit-help__inner">{children}</div>
+			</div>
+		);
+	}
 }

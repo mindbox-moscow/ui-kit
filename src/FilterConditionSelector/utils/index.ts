ee6801fcf0714c847c6ec1dd872474673863c090
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-02-28 15:01:12 +0700

    Реализован первый сценарий работы:  - Если в фильтре только выпадашки (или ничего) - фокус сразу на “добавить”  - Если в фильтре только выпадашки, но фильтр нельзя применить пока не выберешь что-то в одной из выпадшек - фокус на последнюю (?) выпадашку

--- src/FilterConditionSelector/utils/index.ts
+++ src/FilterConditionSelector/utils/index.ts
@@ -4,12 +4,23 @@ export const setNextFocus = () => {
 	const filterDetails = document.querySelector(".kit-filter-details");
 
 	if (filterDetails) {
+		const buttonDisabled = filterDetails.querySelector(
+			'button:disabled:not([tabindex="-1"])'
+		);
 		const inputText = filterDetails.querySelector(
-			'input[type="text"], input[type="number"]'
+			'input[type="text"]:not(:disabled), input[type="number"]:not(:disabled), button:not(:disabled):not([tabindex="-1"])'
 		) as HTMLElement;
 
 		if (inputText) {
 			inputText.focus();
+		} else if (buttonDisabled) {
+			const selectAll = filterDetails.querySelectorAll(".kit-selectR");
+			const selectLast =
+				selectAll &&
+				(selectAll[selectAll.length - 1] as HTMLElement | null);
+
+			// tslint:disable-next-line: no-unused-expression
+			selectLast && selectLast.focus();
 		} else {
 			const elements = getFocusableElements(filterDetails as HTMLElement);
 

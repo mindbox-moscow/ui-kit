f9001e97af45af2d07657ce339835d729fdab81c
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-06-03 16:01:15 +0700

    Выпадашка по системному имени не влезает в экран

--- src/OverflowVisibleContainer/OverflowVisibleContainer.tsx
+++ src/OverflowVisibleContainer/OverflowVisibleContainer.tsx
@@ -3,7 +3,10 @@ import * as React from "react";
 import { Portal } from "../Portal";
 
 import "./OverflowVisibleContainer.scss";
-import { OverflowVisibleFixedContext } from "./OverflowVisibleContext";
+import {
+	OverflowVisibleFixedContext,
+	ScrollableContainerContext
+} from "./OverflowVisibleContext";
 
 export interface IProps {
 	parentRef: React.RefObject<HTMLElement>;
@@ -17,28 +20,79 @@ export const OverflowVisibleContainer = React.forwardRef<Ref, IProps>(
 	(props, ref) => {
 		const { parentRef, className, children } = props;
 
+		const currentRef = React.useRef<HTMLDivElement>(null);
+		const containerRef =
+			(ref as React.RefObject<HTMLDivElement>) || currentRef;
+		const ticking = React.useRef(false);
+
 		const isFixed = React.useContext(OverflowVisibleFixedContext);
+		const scrollableContainer = React.useContext(
+			ScrollableContainerContext
+		);
 
-		let isLoaded = false;
-		let positionLeft = 0;
-		let positionTop = 0;
+		const [isLoaded, setIsLoaded] = React.useState(false);
+		const [positionLeft, setPositionLeft] = React.useState(0);
+		const [positionTop, setPositionTop] = React.useState(0);
 
-		if (parentRef && parentRef.current) {
-			const {
-				top,
-				height,
-				left
-			} = parentRef.current.getBoundingClientRect();
+		const calculateTopPosition = () => {
+			if (
+				containerRef &&
+				containerRef.current &&
+				scrollableContainer &&
+				scrollableContainer.current
+			) {
+				ticking.current = false;
 
-			positionLeft = left + pageXOffset;
-			positionTop = isFixed ? top + height : top + pageYOffset + height;
-			isLoaded = true;
-		}
+				containerRef.current.style.transform = `translateY(${positionTop -
+					scrollableContainer.current.scrollTop}px)`;
+				containerRef.current.style.zIndex = `9999`;
+			}
+		};
+
+		const requestCalculate = () => {
+			if (!ticking.current) {
+				requestAnimationFrame(calculateTopPosition);
+			}
+
+			ticking.current = true;
+		};
+
+		React.useEffect(() => {
+			if (parentRef && parentRef.current) {
+				const {
+					left,
+					top,
+					height
+				} = parentRef.current.getBoundingClientRect();
+
+				setPositionLeft(left + pageXOffset);
+				setPositionTop(
+					isFixed ? top + height : top + pageYOffset + height
+				);
+				setIsLoaded(true);
+
+				if (scrollableContainer && scrollableContainer.current) {
+					scrollableContainer.current.addEventListener(
+						"scroll",
+						requestCalculate
+					);
+				}
+			}
+
+			return () => {
+				if (scrollableContainer && scrollableContainer.current) {
+					scrollableContainer.current.removeEventListener(
+						"scroll",
+						requestCalculate
+					);
+				}
+			};
+		}, []);
 
 		return (
 			<Portal>
 				<div
-					ref={ref}
+					ref={containerRef}
 					className={cn("kit-overflow-visiblecontainer", className, {
 						"kit-overflow-visiblecontainer_fixed": isFixed
 					})}
--- src/OverflowVisibleContainer/OverflowVisibleContext.tsx
+++ src/OverflowVisibleContainer/OverflowVisibleContext.tsx
@@ -1,3 +1,6 @@
 import * as React from "react";
 
+export const ScrollableContainerContext = React.createContext<React.RefObject<
+	HTMLElement
+> | null>(null);
 export const OverflowVisibleFixedContext = React.createContext<boolean>(false);

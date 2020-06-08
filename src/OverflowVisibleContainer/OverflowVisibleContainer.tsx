67d3bd4ee2d751f98a44691aa6064f97e211682b
Вадим Мишхожев <vadim.stug@gmail.com>
2020-06-08 20:32:33 +0700

    [RFC][issue-981]Выпадашка по системному имени не влезает в экран (#1020)
    * Удалил пропс childRef переделал на forwardRef
    Совместил компонент SelectDropMain с SelectSearchList
    Удалил внутренний стейт show в компоненте Dropdown, чтобы получать состояние Dropdown извне
    
    * Добавил новой компонент AsyncSearchSelectBase
    Добавил в зависимости  react-infinite-scroller
    
    * Перенес еще два компонента из фронта
    
    * Исправил стили для "с/по"
    
    * Обновил минорную версию
    
    * Исправлены замечания
    
    * fix
    
    * fix
    
    * Вынес отдельно пропсы для компонентов
    
    * Установил булевые значения в дефолтное состояние
    
    * пропсы в функции makeItemsComponents переделал на объект
    
    * Переименовал файл
    Исправил баг
    
    * Исправлены баги
    
    * Удалил стили
    
    * мемоизирована загрузка списка, исправлены замечания
    
    * мемоизирована загрузка списка, исправлены замечания
    
    * forward
    
    * merge
    
    * Исправлено замечание
    
    * fix merge
    
    * fix merge
    
    * У вкладки шд "съедается" верхняя часть
    
    Исправил баг + некоторые ошибки после мержа
    Исправлены стили
    
    * Исправлно замечание
    
    * Исправлены стили
    
    * fix
    
    * fix
    
    * fix
    
    * Выпадашка по системному имени не влезает в экран
    
    * Исправлены замечания
    
    * Исправлено неверное отображение в fixed
    Добавил перерасчеты при ресайзе, чтобы при изменении маштаба dropdown оставался на своем месте

--- src/OverflowVisibleContainer/OverflowVisibleContainer.tsx
+++ src/OverflowVisibleContainer/OverflowVisibleContainer.tsx
@@ -1,9 +1,14 @@
 import cn from "classnames";
 import * as React from "react";
+
+import { useDebouncedWindowSize } from "../HOOKs";
 import { Portal } from "../Portal";
 
 import "./OverflowVisibleContainer.scss";
-import { OverflowVisibleFixedContext } from "./OverflowVisibleContext";
+import {
+	OverflowVisibleFixedContext,
+	ScrollableContainerContext
+} from "./OverflowVisibleContext";
 
 export interface IProps {
 	parentRef: React.RefObject<HTMLElement>;
@@ -17,28 +22,93 @@ export const OverflowVisibleContainer = React.forwardRef<Ref, IProps>(
 	(props, ref) => {
 		const { parentRef, className, children } = props;
 
+		const debouncedWindowSize = useDebouncedWindowSize();
+
+		const currentRef = React.useRef<HTMLDivElement>();
+		const containerRef =
+			(ref as React.RefObject<HTMLDivElement>) || currentRef;
+		const ticking = React.useRef(false);
+
 		const isFixed = React.useContext(OverflowVisibleFixedContext);
+		const scrollableContainer = React.useContext(
+			ScrollableContainerContext
+		);
 
-		let isLoaded = false;
-		let positionLeft = 0;
-		let positionTop = 0;
+		const [isLoaded, setIsLoaded] = React.useState(false);
+		const [positionLeft, setPositionLeft] = React.useState(0);
+		const [positionTop, setPositionTop] = React.useState(0);
 
-		if (parentRef && parentRef.current) {
-			const {
-				top,
-				height,
-				left
-			} = parentRef.current.getBoundingClientRect();
+		const calculateTopPosition = () => {
+			if (
+				containerRef &&
+				containerRef.current &&
+				scrollableContainer &&
+				scrollableContainer.current
+			) {
+				ticking.current = false;
 
-			positionLeft = left + pageXOffset;
-			positionTop = isFixed ? top + height : top + pageYOffset + height;
-			isLoaded = true;
-		}
+				containerRef.current.style.willChange = "transform";
+				containerRef.current.style.transform = `translate3d(0, ${positionTop -
+					scrollableContainer.current.scrollTop}px, 0)`;
+				containerRef.current.style.zIndex = `9999`;
+			}
+		};
+
+		const requestCalculate = () => {
+			if (!ticking.current) {
+				requestAnimationFrame(calculateTopPosition);
+			}
+
+			ticking.current = true;
+		};
+
+		const calculatePosition = () => {
+			if (parentRef && parentRef.current) {
+				const {
+					left,
+					top,
+					height
+				} = parentRef.current.getBoundingClientRect();
+
+				setPositionLeft(left + pageXOffset);
+				setPositionTop(
+					isFixed ? top + height : top + pageYOffset + height
+				);
+			}
+		};
+
+		React.useEffect(
+			() => {
+				calculatePosition();
+			},
+			[debouncedWindowSize, isFixed]
+		);
+
+		React.useEffect(() => {
+			calculatePosition();
+			setIsLoaded(true);
+
+			if (scrollableContainer && scrollableContainer.current) {
+				scrollableContainer.current.addEventListener(
+					"scroll",
+					requestCalculate
+				);
+			}
+
+			return () => {
+				if (scrollableContainer && scrollableContainer.current) {
+					scrollableContainer.current.removeEventListener(
+						"scroll",
+						requestCalculate
+					);
+				}
+			};
+		}, []);
 
 		return (
 			<Portal>
 				<div
-					ref={ref}
+					ref={containerRef}
 					className={cn("kit-overflow-visiblecontainer", className, {
 						"kit-overflow-visiblecontainer_fixed": isFixed
 					})}

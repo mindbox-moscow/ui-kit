dabeecbbe1d24951ecdb738fa94ac45f11e75ad9
Вадим Мишхожев <vadim.stug@gmail.com>
2020-06-08 20:38:52 +0700

    [RFC][issue-1004]Поправить иконку с валидацией (#1021)
    * Исправлено отображение тултипа
    
    * Исправлено положение тултипа
    
    * fix

--- src/BlockComponents/components/Wrapper/Wrapper.tsx
+++ src/BlockComponents/components/Wrapper/Wrapper.tsx
@@ -1,7 +1,4 @@
-import cn from "classnames";
 import * as React from "react";
-
-import "./Wrapper.scss";
 
 interface IProps {
 	tag?: "div" | "span";
@@ -12,13 +9,5 @@ export const Wrapper: React.FC<IProps> = props => {
 
 	const Tag = tag;
 
-	return (
-		<Tag
-			className={cn("kit-block-wrapper", {
-				"kit-block-wrapper_inline": tag === "span"
-			})}
-		>
-			{children}
-		</Tag>
-	);
+	return <Tag className="kit-block-wrapper">{children}</Tag>;
 };

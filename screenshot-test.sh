1ca1dcf6cf3072c084d42522593af59f7f9c9e1f
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-06-23 21:42:30 +0700

    fixed prefix for run docker

--- screenshot-test.sh
+++ screenshot-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build . -t ui-kit:screenshot-test
-docker run -it --rm --mount type=bind,source="$(pwd)",target=/app ui-kit:screenshot-test
+docker run -t --rm --mount type=bind,source="$(pwd)",target=/app ui-kit:screenshot-test

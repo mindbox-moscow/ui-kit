ca76ac6bfb7c090b3083ef7012a7e60fa09aa8b9
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-06-23 21:00:44 +0700

    test config

--- jest/setupTests.ts
+++ jest/setupTests.ts
@@ -1,11 +1,6 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { toMatchImageSnapshot } from "jest-image-snapshot";
 
 window.HTMLElement.prototype.scrollIntoView = () => {};
 
 configure({ adapter: new Adapter() });
-
-jest.setTimeout(10000);
-
-expect.extend({ toMatchImageSnapshot });

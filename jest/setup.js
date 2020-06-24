849e504989f68f45198451e4cbefbabea6829729
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-06-24 14:11:31 +0700

    remove package jest-docker-puppeteer

--- jest/setup.js
+++
@@ -1,6 +0,0 @@
-const { setup: setupPuppeteer } = require("jest-puppeteer-docker");
-
-module.exports = async jestConfig => {
-	// any stuff you need to do can go here
-	await setupPuppeteer(jestConfig);
-};
--- jest/teardown.js
+++
@@ -1,6 +0,0 @@
-const { teardown: teardownPuppeteer } = require("jest-puppeteer-docker");
-
-module.exports = async jestConfig => {
-	await teardownPuppeteer(jestConfig);
-	// any stuff you need to do can go here
-};

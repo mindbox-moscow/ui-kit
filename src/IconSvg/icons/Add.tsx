6df1b59902591e8aff8a695adc5cc86a26edbedf
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-06-24 16:03:28 +0700

    Изменена иконка

--- src/IconSvg/icons/Add.tsx
+++ src/IconSvg/icons/Add.tsx
@@ -3,7 +3,29 @@ import * as React from "react";
 import { ISvgProps } from "./index";
 
 export const Add = ({ className }: ISvgProps) => (
-	<svg className={className} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
-		<path d="M21 8H3V6H21V8M13.81 16H10V18H13.09C13.21 17.28 13.46 16.61 13.81 16M18 11H6V13H18V11M18 15V18H15V20H18V23H20V20H23V18H20V15H18Z" fill="currentColor" />
+	<svg
+		className={className}
+		width="18"
+		height="18"
+		viewBox="0 0 18 18"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<g transform="translate(1 1)" fill="none" fill-rule="evenodd">
+			<rect
+				stroke="#D9E7FE"
+				opacity=".75"
+				width="16"
+				height="16"
+				rx="3"
+			/>
+			<g
+				stroke="#6A98E1"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+			>
+				<path d="M8 4v8M4 8h8" />
+			</g>
+		</g>
 	</svg>
 );

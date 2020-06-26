e4cdd5237aea1f87558af4f1f5e9ab718fd0832f
Kuzmin Aleksei <kuzmin@mindbox.ru>
2020-06-26 10:29:50 +0300

    ci: fix cleanup script

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -5,4 +5,4 @@ HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "{$1}" https:
 curl -i -X DELETE \
   -H "Accept: application/json" \
   -H "Authorization: JWT $HUB_TOKEN" \
-  https://hub.docker.com/v2/repositories/itmindbox/$2
+  https://hub.docker.com/v2/repositories/itmindbox/dev_ui_kit-$2

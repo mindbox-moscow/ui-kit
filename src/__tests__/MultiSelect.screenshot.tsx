42ebef9b84b5a851533bd03ab3ffbb1f83a6c476
Ilya Vassyutovich <vasutovich@mindbox.ru>
2020-07-29 19:44:51 +0300

    Улучшаю видимость элементов multiselect'а

--- src/__tests__/MultiSelect.screenshot.tsx
+++ src/__tests__/MultiSelect.screenshot.tsx
@@ -11,6 +11,7 @@ describe("MultiSelect", () => {
 	it("Close", async () => {
 		page = await getPage("select/MultiSelectDefault.html");
 		await page.click("div.kit-selectR");
+		await page.click("li.kit-selectR-result.kit-selectR-selected-multi");
 		const image = await page.screenshot();
 
 		expect(image).toMatchImageSnapshot();
